3b621053255595b85c072559fe6ffec6946a6e75
LlucBarri <106800006+LlucBarri@users.noreply.github.com>
2022-06-10 09:01:20 +0200

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://llucbarri.github.io/joc-dos-lluc/](https://llucbarri.github.io/joc-dos-lluc/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/llucbarri/joc-dos-lluc** and import
+
+## Edit this project ![Build status badge](https://github.com/llucbarri/joc-dos-lluc/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/llucbarri/joc-dos-lluc** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/llucbarri/joc-dos-lluc/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,80 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f . . . . . . . . 
+        . . . . . f 1 1 f . . . . . . . 
+        . . . . f 1 1 f 1 f . . . . . . 
+        . . . f 1 1 1 1 1 1 f . . . . . 
+        . . . f f 1 f f 1 f f . . . . . 
+        . . . f 1 1 1 1 1 1 f . . . . . 
+        . . . . f 1 1 f 1 f . . . . . . 
+        . . . . . f 1 1 f . . . . . . . 
+        . . . . . . f f . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, -50)
+    music.pewPew.play()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    ASTEROIDE.destroy()
+    mySprite.destroy(effects.confetti, 500)
+    info.changeLifeBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    mySprite.destroy(effects.fire, 500)
+    scene.cameraShake(4, 500)
+})
+let ASTEROIDE: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+game.splash("BENVINGUTS A L'ESPAI", "Apreta A per començar i B per disparar")
+effects.starField.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . d d . . . . . . . 
+    . . . . . . d d d d . . . . . . 
+    . . . . . . 5 d d 5 . . . . . . 
+    . . . . . . . 2 2 . . . . . . . 
+    . . . . . . 2 2 2 2 . . . . . . 
+    . . . . . 2 2 f f 2 2 . . . . . 
+    . . . . 2 2 2 2 f 2 2 2 . . . . 
+    . . . 2 2 2 2 2 f 2 2 2 2 . . . 
+    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    . . f f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(64, 101)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setStayInScreen(true)
+info.setLife(5)
+game.onUpdateInterval(500, function () {
+    ASTEROIDE = sprites.createProjectileFromSide(img`
+        . . . . . . . . c c c c . . . . 
+        . . . . c c c c c c c c c . . . 
+        . . . c f c c a a a a c a c . . 
+        . . c c f f f f a a a c a a c . 
+        . . c c a f f c a a f f f a a c 
+        . . c c a a a a b c f f f a a c 
+        . c c c c a c c b a f c a a c c 
+        c a f f c c c a b b 6 b b b c c 
+        c a f f f f c c c 6 b b b a a c 
+        c a a c f f c a 6 6 b b b a a c 
+        c c b a a a a b 6 b b a b b a . 
+        . c c b b b b b b b a c c b a . 
+        . . c c c b c c c b a a b c . . 
+        . . . . c b a c c b b b c . . . 
+        . . . . c b b a a 6 b c . . . . 
+        . . . . . . b 6 6 c c . . . . . 
+        `, 0, 50)
+    ASTEROIDE.x += randint(0, scene.screenWidth())
+    ASTEROIDE.setKind(SpriteKind.Enemy)
+})
